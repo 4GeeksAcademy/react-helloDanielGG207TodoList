9de2beb3de3cebd61ccbcc7c9cfac05bbab2f2c0
DanielGG207 <cocorroquiasprogamer@gmail.com>
2025-03-18 15:52:18 +0000

    [proyecto terminado y conectado con fetch]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,105 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { use } from "react";
+
+
+import { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	
+	
+	const [isHover, setisHover] = useState("showF")
+	const [tasks, setTasks] = useState([])
+    const [change, setChange] = useState([true])
+	useEffect (() => { 
+       
+	fetch("https://playground.4geeks.com/todo/users/DanielGG207/"  )
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	.then( (response)=>{
+      return(response.json())
+	})
+	
+ 
+	.then((data)=> {
+		setTasks(data.todos)
+		console.dir(tasks)
+		
+	})
+
+	.catch()
+
+	}, [change])
+  
+	return (
+		
+		<div className="text-container">
+			
+
+            <h1>Todos</h1>
+			<div className="tasks-box bg-white">
+				<input placeholder = "Type your task" onKeyUp={(event) => {
+					if (event.key == "Enter") {
+						let data = {
+							"label": event.target.value,
+							"is_done": false
+						  }
+						console.log(data)
+						fetch("https://playground.4geeks.com/todo/todos/DanielGG207" , {
+                        method: "POST",
+						body:JSON.stringify(data),
+						headers: {
+							"Content-Type": "application/json"
+						  }
+						})
+
+						.then(()=> {setChange(!change)})
+
+						.then(()=> {})
+
+						.catch()
+						setTasks((lastValue) => 
+							{ return ([...lastValue, event.target.value])})
+						
+						
+					}
+						
+					}}></input>
+                
+				<h2>{tasks.map((task) => 
+					{
+						
+						return (<div className="task-container" onMouseEnter={()=> {setisHover("")}} onMouseLeave={()=>{setisHover ("showF")}}>
+						 {task.label} <div className={isHover + " delete"} data-id= {task.id} 
+						 
+						onClick={(event) => {
+							let taskIndex = Number(event.currentTarget.dataset.id);
+							fetch("https://playground.4geeks.com/todo/todos/" + taskIndex , {method: "DELETE"})
+
+							.then(()=> {setChange(!change)})
+
+							.then()
+
+							.catch()
+
+						}}> 
+						 <i className="fa-solid fa-trash"></i>  </div> 
+
+					</div>)})}
+					
+					
+					</h2>
+
+					
+					{console.dir(tasks)}
+				
+			</div>
+
+			<div>
+				
+			</div>
 		</div>
 	);
 };
